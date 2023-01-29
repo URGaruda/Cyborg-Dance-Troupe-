9ade78055b6feba306d1e86f70b701aabe36a21a
URGaruda <daba.indigo@gmail.com>
2023-01-29 18:52:10 +0100

    Ajout de la méthode de connaisance de collision

--- Simulation2.py
+++ Simulation2.py
@@ -10,7 +10,9 @@ class Simulation :
         self.dexter=Robot(x,y,v,25/2,60,dirX,dirY) #Je suppose ici que le Robot fait 25 cm de long et qu'il a un angle de vue de 60° . Vous pouvez les mettres en paramètres si vous préféré comme ça 
         self.liste_obstacle=obstacle
         self.pas_temps=temps
-
     
-    
-    
+    def colision(self):
+        for obj in self.liste_obstacle :
+            if not ( Vecteur.p_scalaire(Vecteur(self.dexter.x,self.dexter.y),Vecteur(obj.x,obj.y)) > self.dexter.R + obj.R ) : # si y'a colision iol renvoie vrai 
+                return True
+        return False 
