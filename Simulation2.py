37ceab2e8b1d60eac5c758e4f4bb74ab9edb40c7
URGaruda <daba.indigo@gmail.com>
2023-01-30 13:50:28 +0100

    modification de collision

--- Simulation2.py
+++ Simulation2.py
@@ -4,8 +4,8 @@ from Robot2 import Robot
 from Obstacle import Obstacle
 
 class Simulation : 
-    Ymax=400 #en cm
-    Xmax=355 #en cm 
+    Ymax=400.0 #en cm
+    Xmax=355.0 #en cm 
     def __init__(self,x,y,v,dirX,dirY,temps,obstacle):
         self.dexter=Robot(x,y,v,25/2,60,dirX,dirY) #Je suppose ici que le Robot fait 25 cm de long et qu'il a un angle de vue de 60° . Vous pouvez les mettres en paramètres si vous préféré comme ça 
         self.liste_obstacle=obstacle
@@ -13,7 +13,9 @@ class Simulation :
     
     def colision(self):
         for obj in self.liste_obstacle :
-            if not ( Vecteur.p_scalaire(Vecteur(self.dexter.x,self.dexter.y),Vecteur(obj.x,obj.y)) > self.dexter.R + obj.R ) : # si y'a colision iol renvoie vrai 
+            print("j'y fus")
+            if not(math.sqrt(((obj.x-self.dexter.x)**2)+((obj.y-self.dexter.y)**2)) > obj.R+self.dexter.R): # si y'a colision il renvoie vrai 
                 self.dexter.stop()
                 return True
         return False 
+
