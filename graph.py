a7f52b0de2b76ec03392414276ad6ba05c89d72a
URGaruda <daba.indigo@gmail.com>
2023-02-01 01:04:22 +0100

    changement sur simulation

--- graph.py
+++ graph.py
@@ -1,10 +1,11 @@
+import tkinter as tk
 import math
 import time 
 from Vecteur import Vecteur
 from Robot2 import Robot 
 
 class Obstacle : 
-    def __init__(self,x,y,R):
+    def _init_(self,x,y,R):
         self.x=x
         self.y=y
         self.R=R
@@ -12,7 +13,7 @@ class Obstacle :
 class Simulation : 
     Ymax=400.0 #en cm
     Xmax=355.0 #en cm 
-    def __init__(self,x,y,v,dirX,dirY,temps,obstacle):
+    def _init_(self,x,y,v,dirX,dirY,temps,obstacle):
         self.dexter=Robot(x,y,v,25/2,60,dirX,dirY) #Je suppose ici que le Robot fait 25 cm de long et qu'il a un angle de vue de 60° . Vous pouvez les mettres en paramètres si vous préféré comme ça 
         self.liste_obstacle=obstacle
         self.pas_temps=temps
@@ -20,12 +21,12 @@ class Simulation :
     def collision(self,vect):
         """ Determine si le Robot Dexter est où pas en collision avec un des obstacles du terrain """
         for obj in self.liste_obstacle :
-            if not(math.sqrt(((obj.x-vect.x)**2)+((obj.y-vect.y)**2)) > obj.R+vect.R): # si y'a colision il renvoie vrai 
+            if not(math.sqrt(((obj.x-vect.x)*2)+((obj.y-vect.y)*2)) > obj.R+vect.R): # si y'a colision il renvoie vrai 
                 return True
         return False 
     def hors_terrain(self,robot):
         """ Prends un robot et vérifie s'il est toujours sur le terrain """
-        if(robot.x<0.0 or robot.x >= Simulation.Xmax or robot.y < 0.0 or robot.y >= Simulation.Ymax ):
+        if(robot.x<0 or robot.x >= Simulation.Xmax or robot.y < 0 or robot.y >= Simulation.Ymax ):
             return True 
         return False 
     def senseur(self):
@@ -42,63 +43,44 @@ class Simulation :
                 return pas 
         return -1 
 
-    def affichage(self):
-        print("+", end='')
-        for j in range(int(self.Ymax)+1):
-            print("-", end='')
-        print("+\n")
-        for i in range(int(self.Xmax)+1):
-            print("|", end='')
-            #for j in range(self.Ymax):
-            print("|\n")
-        print("+", end='')
-        for j in range(int(self.Ymax)+1):
-            print("-", end='')
-        print("+\n")
-    def simulation_carre(self,pas):
+
+    def simulation_carre(self,pas,canvas):
         """ Fait faire un carré au robot de la distance "dist" au robot sur le terrain (0.0,Xmax) en x et (0.0,Ymax) """
-        print("ok")
-        print(self.dexter.dir.x,",",self.dexter.dir.y)
         for i in range(4):
-            direction=self.dexter.dir.copie(self.dexter.dir)
-            vect_v=self.dexter.dir.mult_par_un_scalaire(direction,(self.dexter.v*self.pas_temps))
-            print("vect_v.x",vect_v.x,"et vect_v.y",vect_v.y)
             for j in range(pas):
-                print("ok")
-                print(self.dexter.x,",",self.dexter.y) 
-                self.dexter.x+=vect_v.x
-                self.dexter.y+=vect_v.y
+                canvas.create_oval(self.dexter.x - (10/2)*2, self.dexter.y - (10/2)*2,
+                                self.dexter.x + (10/2)*2, self.dexter.y + (10/2)*2, fill="red")
+                vect_v=self.dexter.dir.mult_par_un_scalaire(self.dexter.dir,(self.dexter.v/self.pas_temps))
+                self.dexter.x=vect_v.x
+                self.dexter.y=vect_v.y
                 """if(self.hors_terrain(self.dexter)):
                     print("Dexter est sorti du terrain")
                     return
                 """ 
-                if(self.collision(Vecteur(self.dexter.x,self.dexter.y ))):
+                if(self.collision):
                     print("Il y a eu collision")
                     return 
                 if(self.senseur()==1):
-                    print("Obstacle à proximité")
                     time.sleep(1)
                     break 
                 time.sleep(0.5)
-            print("coucou")
             self.dexter.dir.rotation_anti_horaire(math.pi/2)
             time.sleep(2)
         print("simulation fini")
-for i in [] :
-    print("aaah")
-#tester la classe        
-try:
-    sim=Simulation(104.29,145.96,2,56,24,0.10,[])
-except ValueError as erreur :
-    print(erreur)    
-def update():
-    sim=Simulation(124.29,196.96,4.2,3,8,0.020,[])
-    sim.simulation_carre(10)
-update()
+        
+root = tk.Tk()
+root.title("Simulation du mouvement du Robot en carré")
 
-            
-            
+canvas = tk.Canvas(root, width=500, height=500)
+canvas.pack()
+"""obs1=Obstacle(3,4,3)
+obs2=Obstacle(5,7,3)
+obs3=Obstacle(6,8,3)
+L0.append(obs1)
+L0.append(obs2)
+L0.append(obs3)"""
+L0 =[]
+simul = Simulation(1,1,5,3,3,0.1,L0)
+simul.simulation_carre(10,canvas)
 
-
-
-
+root.mainloop()
