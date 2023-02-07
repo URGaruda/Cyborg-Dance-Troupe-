a619b63abd95bf10da7d50ff39d177b8c448ebf2
Akram Boudouoaur <boudouaouraccram@gmail.com>
2023-02-07 09:45:48 +0100

    Akram modified the simulation and the robot class

--- Robot2.py
+++ Robot2.py
@@ -1,6 +1,4 @@
 import math
-import copy
-from Vecteur import Vecteur 
 
 class Robot:
     """
@@ -10,13 +8,14 @@ class Robot:
         x (float): la position x du robot.
         y (float): la position y du robot.
         orientation (float): orientation du robot en radians.
+        rayon_robot(float): le rayon du robot.
         rayon_roue (float): le rayon des deux roues.
         distance_roues (float): la distance entre les deux roues du robot.
         vitesse_roue_gauche (float): vitesse angulaire de la roue gauche du robot en radians par seconde.
         vitesse_roue_droite (float): vitesse angulaire de la roue droite du robot en radians par seconde.
         historique (list): une liste de tuples de la position x, y du robot à chaque instant.
     """
-    def __init__(self, x, y, orientation, rayon_roue, distance_roues):
+    def __init__(self, x, y, orientation, rayon_robot, rayon_roue, distance_roues):
         """
         Initialise un objet de la classe Robot avec les paramètres donnés.
         """
@@ -24,6 +23,7 @@ class Robot:
         self.x = x
         self.y = y
         self.orientation = orientation
+        self.rayon_robot=rayon_robot
         self.rayon_roue = rayon_roue
         self.distance_roues = distance_roues
         self.vitesse_roue_gauche = 0
@@ -37,11 +37,11 @@ class Robot:
         self.vitesse_roue_gauche = vitesse_roue_gauche
         self.vitesse_roue_droite = vitesse_roue_droite
 
-    def update_position(self, delta_time):
+    def deplacement(self, delta_time):
         """
         Met à jour la position et l'orientation du robot en fonction des vitesses de ses roues.
         """
-        vitesse_moyenne = (self.vitesse_roue_gauche + self.vitesse_roue_droite) / 2 #vitesse angulaire moyenne
+        vitesse_moyenne = (self.vitesse_roue_gauche + self.vitesse_roue_droite) / 2 
         vitesse_moyenne = vitesse_moyenne / (2 * math.pi * self.rayon_roue) #vitesse lineaire moyenne
         delta_x = vitesse_moyenne * math.cos(self.orientation) * delta_time #trignometry
         delta_y = vitesse_moyenne * math.sin(self.orientation) * delta_time #trigonometry
@@ -54,39 +54,43 @@ class Robot:
         self.orientation += delta_orientation #calcule la nouvelle orientation
         
         self.historique.append((self.x, self.y))
+    
+    def check_collision(self, arene_x, arene_y, obstacles):
+        """
+        Vérifie s'il y a une collision entre le robot et les bords de l'arène/ obstacle
+        """
+        # Vérifie une collision avec les bords de l'arène
+        if self.x - self.rayon_robot < 0 or self.x + self.rayon_robot > arene_x:
+            return True
+        if self.y - self.rayon_robot < 0 or self.y + self.rayon_robot > arene_y:
+            return True
+        
+        # Vérifie une collision avec les obstacles
+        for obstacle in obstacles:
+            distance = math.sqrt((self.x - obstacle.x)**2 + (self.y - obstacle.y)**2)
+            if distance < self.rayon_robot + obstacle.rayon:
+                return True
+        
+        return False
 
-    def copie(self):
-        """Fait une copie du robot """
-        robot=copy.copy(self)
-        return robot
-
+    
+    '''def senseur(self):
+        """ Determine s'il y a un obstacle sur la direction du robot et renvoie le nombre de pas s'il l'a trouvé un
+        obstacle ou sinon -1 s'il l'a rien trouvé """
+        robot=self.dexter.copie() # crée une copie qui va faire des 
+        pas=0
+        while not(self.hors_terrain(robot)) :
+            vect_v=self.dexter.dir.mult_par_un_scalaire((self.dexter.v*self.pas_temps))
+            robot.x=vect_v.x
+            robot.y=vect_v.y
+            pas+=1
+            if(self.collision(robot)):
+                return pas 
+        return -1 '''
     
     
     
 
 
-"""
-#tester la classe        
-try:
-    robot=Robot(1,1,20,10,50,5,3) 
-    print(robot.dir.x,",",robot.dir.y)  
-    robot.stop()
-    print(robot.v)
-    robot.acceleration(30)
-    print(robot.v)
-    robot.freinage(15)
-    print(robot.v)
-    robot2=robot.copie()
-    print("c'est la copie")
-    print(robot2.v)
-    print(robot.dir.x,",",robot.dir.y) 
-    
-except ValueError as erreur :
-    print(erreur) 
-    robot.stop()
-    print(robot.v)
-    robot.acceleration(30)
-    print(robot.v)
-"""
 
 
--- Simulation2.py
+++ Simulation2.py
@@ -1,103 +1,81 @@
 import math
-import time 
+import matplotlib.pyplot as plt
 from Robot2 import Robot 
 import random
 
+
 class Obstacle : 
-    def __init__(self,x,y,R):
+    def __init__(self,x,y,rayon):
         self.x=x
         self.y=y
-        self.R=R
+        self.rayon=rayon
 
 class Simulation : 
-    Ymax=400.0 #en cm
-    Xmax=355.0 #en cm 
-    def __init__(self,x,y,dirX,dirY,temps,obstacle):
-        self.dexter=Robot(x,y,25/2,60,dirX,dirY) #Je suppose ici que le Robot fait 25 cm de long et qu'il a un angle de vue de 60° . Vous pouvez les mettres en paramètres si vous préféré comme ça 
-        self.liste_obstacle=obstacle
-        self.pas_temps=temps
+       
+    def __init__(self,obstacles):
+        self.robot=Robot(x=0, y=0, orientation=0, rayon_robot=0.3, rayon_roue=0.05, distance_roues=0.2) 
+        self.obstacles=obstacles
+        self.arene_longueur = 100
+        self.arene_largeur = 100
     
-    def collision(self,robot):
-        """ Determine si le Robot Dexter est où pas en collision avec un des obstacles du terrain """
-        for obj in self.liste_obstacle :
-            return not(math.sqrt(((obj.x-robot.x)**2)+((obj.y-robot.y)**2)) > obj.R+robot.R) # si y'a colision il renvoie vrai 
+    def trace_cote_carre(self,robot, vitesse, dt, taille_carre):
+        """
+        Trace un coté du carré avec le robot et faire une rotation pour se préparer à tourner.
     
-    def hors_terrain(self,robot):
-        """ Prends un robot et vérifie s'il est toujours sur le terrain """
-        return (robot.x<0.0 or robot.x >= Simulation.Xmax or robot.y < 0.0 or robot.y >= Simulation.Ymax )
-            
-    def senseur(self):
-        """ Determine s'il y a un obstacle sur la direction du robot et renvoie le nombre de pas s'il l'a trouvé un
-        obstacle ou sinon -1 s'il l'a rien trouvé """
-        robot=self.dexter.copie() # crée une copie qui va faire des 
-        pas=0
-        while not(self.hors_terrain(robot)) :
-            vect_v=self.dexter.dir.mult_par_un_scalaire((self.dexter.v*self.pas_temps))
-            robot.x=vect_v.x
-            robot.y=vect_v.y
-            pas+=1
-            if(self.collision(robot)):
-                return pas 
-        return -1 
-    def update_position(self, time_delta):
-        avg_speed = (self.dexter.left_wheel_speed + self.dexter.right_wheel_speed) / 2
-        avg_speed = avg_speed / (2 * math.pi * self.dexter.wheel_radius)
-        delta_x = avg_speed * math.cos(self.dexter.direction) * time_delta
-        delta_y = avg_speed * math.sin(self.dexter.direction) * time_delta
-        self.x += delta_x
-        self.y += delta_y
+        Paramètres:
+            robot (Robot): objet de la classe Robot.
+            vitesse (float): valeur de vitesse angulaire appliquée au roues.
+            dt (float): pas de temps de la simulation.
+            taille_carre (float): taille du côté du carré en mètres.
         
-        # Calculate the rotation using the formula for angular velocity
-        angular_speed = (self.dexter.right_wheel_speed - self.dexter.left_wheel_speed) / (2 * math.pi * self.dexter.wheel_base)
-        delta_direction = angular_speed * time_delta
-        self.dexter.direction += delta_direction
+        Retourne:
+            list: une liste de tuples représentant la position x, y du robot à chaque instant.
+        """
+        robot.set_vitesse(vitesse, vitesse)
+        duree = taille_carre / ((robot.vitesse_roue_gauche + robot.vitesse_roue_droite) / 2) * robot.rayon_roue * 2 * math.pi
+        nombre_pas = int(duree / dt)
+        #le robot trace un coté du carré
+        for j in range(nombre_pas):
+            robot.deplacement(dt)
+            if robot.check_collision(self.arene_longueur,self.arene_largeur,self.obstacles):
+                robot.set_vitesse(0,0)
+                return robot.historique
 
-        # Record the current position in history
-        self.history.append((self.x, self.y))
-    
-    def marche_arrierre(self):
-        self.dexter.set_speeds(-self.dexter.left_wheel_speed,-self.dexter.right_wheel_speed) #inverse la vitesse des deux roues pour faire une marche arrière 
-    def simulation_carre(self,distance,time_delta):#prend une distance à parcourir 
-        """ Fait faire un carré au robot dont les cotés sont égaux à la distance"""
-        self.dexter.set_speeds(10, 10)
-        get_dist=0.0
-        state=0
-        b=False # Variable qui permet de savoir si on a tourné dans la boucle 
-        while not(self.collision(self.dexter) or self.hors_terrain(self.dexter) or state>4):
-            if(get_dist>=distance):
-                b=True
-                self.dexter.set_speeds(-self.dexter.left_wheel_speed,self.dexter.right_wheel_speed)
-                time_to_turn = math.pi / 2
-                angular_speed = (self.dexter.right_wheel_speed - self.dexter.left_wheel_speed) / (2 * math.pi * self.dexter.wheel_base)
-                turn_time = abs(time_to_turn / angular_speed) # Time needed to turn
-                num_steps = int(turn_time / time_delta)
-                i=0
-                while i<num_steps :
-                    self.update_position(time_delta)
-                    self.dexter.history.append((self.dexter.x,self.dexter.y))
-                    i+=1
-                state+=1
-            if(b):
-                self.dexter.set_speeds(-self.dexter.left_wheel_speed,self.dexter.right_wheel_speed)
-            self.update_position(time_delta)
-            get_dist+=math.sqrt((self.dexter.x-self.dexter.history[len-2][0])**2+(self.dexter.y-self.dexter.history[len-2][1])**2) # ajout de la distance entre l'ancienne position et la nouvelle position 
+        
+        robot.set_vitesse(vitesse, -vitesse) #pour pouvoir tourner
+        angle = math.pi / 2
+        vitesse_angulaire = (robot.vitesse_roue_droite - robot.vitesse_roue_gauche) / (2 * math.pi * robot.distance_roues)
+        temps_tourner = abs(angle / vitesse_angulaire) #temps nécessaire pour finir le tour
+        nombre_pas = int(temps_tourner / dt)
+        #rotation du robot autour de son axe central
+        for j in range(nombre_pas):
+            robot.deplacement(dt)
+
+        return robot.historique
+
+    def affichage(self):
+        fig, ax = plt.subplots()
+        l_obstacles=[plt.Circle((obs.x,obs.y),obs.rayon)for obs in obstacles]
+        for o in l_obstacles:
+            ax.add_patch(o)
+        l_robot=[plt.Circle((x, y), 0.3, color='r') for (x,y) in simulation_historique]
+        for r in l_robot:
+            ax.add_patch(r)
+        ax.set_xlim(0, 100)
+        ax.set_ylim(0, 100)
+        plt.show()
+
+robot = Robot(x=50, y=50, orientation=0, rayon_robot=0.3, rayon_roue=0.05, distance_roues=0.2) 
+obstacles= [Obstacle(random.uniform(0,100),random.uniform(0,100),random.uniform(0.3,1))for i in range (50) ]
+simulation=Simulation(obstacles)
+
+while True:
+    simulation_historique = simulation.trace_cote_carre(robot, 10, 0.0001, 10)
+    simulation.affichage()
 
             
     
 
-for i in [] :
-    print("aaah")
-#tester la classe
-"""        
-try:
-    sim=Simulation(104.29,145.96,2,56,24,0.10,[])
-except ValueError as erreur :
-    print(erreur)    
-def update():
-    sim=Simulation(124.29,196.96,4.2,3,8,0.20,[])
-    sim.simulation_carre(10)
-update()
-"""
 
 
            
