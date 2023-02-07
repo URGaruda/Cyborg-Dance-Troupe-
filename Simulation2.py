8b4573d200b266c3b644d11bf19f820d24564d24
URGaruda <101092183+URGaruda@users.noreply.github.com>
2023-02-07 13:56:40 +0100

    Update Simulation2.py

--- Simulation2.py
+++ Simulation2.py
@@ -52,6 +52,31 @@ class Simulation :
             robot.deplacement(dt)
 
         return robot.historique
+    
+    def simulation_carre(self,distance,time_delta):#prend une distance à parcourir 
+        """ Fait faire un carré au robot dont les cotés sont égaux à la distance"""
+        self.dexter.set_speeds(10, 10)
+        get_dist=0.0
+        state=0
+        b=False # Variable qui permet de savoir si on a tourné dans la boucle 
+        while not(self.collision(self.dexter) or self.hors_terrain(self.dexter) or state>4):
+            if(get_dist>=distance):
+                b=True
+                self.dexter.set_speeds(-self.dexter.left_wheel_speed,self.dexter.right_wheel_speed)
+                time_to_turn = math.pi / 2
+                angular_speed = (self.dexter.right_wheel_speed - self.dexter.left_wheel_speed) / (2 * math.pi * self.dexter.wheel_base)
+                turn_time = abs(time_to_turn / angular_speed) # Time needed to turn
+                num_steps = int(turn_time / time_delta)
+                i=0
+                while i<num_steps :
+                    self.update_position(time_delta)
+                    self.dexter.history.append((self.dexter.x,self.dexter.y))
+                    i+=1
+                state+=1
+            if(b):
+                self.dexter.set_speeds(-self.dexter.left_wheel_speed,self.dexter.right_wheel_speed)
+            self.update_position(time_delta)
+            get_dist+=math.sqrt((self.dexter.x-self.dexter.history[len-2][0])**2+(self.dexter.y-self.dexter.history[len-2][1])**2) # ajout de la distance entre l'ancienne position et la nouvelle position 
 
     def affichage(self):
         fig, ax = plt.subplots()
