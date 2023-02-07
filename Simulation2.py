66dc643927a81943cf91ec84d3cf0a7768e456a2
URGaruda <daba.indigo@gmail.com>
2023-02-07 17:18:33 +0100

    c'est presque ça

--- Simulation2.py
+++ Simulation2.py
@@ -12,25 +12,25 @@ class Obstacle :
 
 class Simulation : 
        
-    def __init__(self,obstacles):
-        self.robot=Robot(x=0.0, y=0.0, orientation=0, rayon_robot=0.3, rayon_roue=0.05, distance_roues=0.2) 
+    def __init__(self,robot,obstacles):
+        self.robot=robot
         self.obstacles=obstacles
-        self.arene_longueur = 400.0
-        self.arene_largeur = 400.0
+        self.arene_longueur = 50.0
+        self.arene_largeur = 50.0
 
-    def check_collision(self,robot):
+    def check_collision(self):
         """
         Vérifie s'il y a une collision entre le robot et les bords de l'arène/ obstacle
         """
         # Vérifie une collision avec les bords de l'arène
-        if self.robot.x - self.robot.rayon_robot < 0 or self.x + self.robot.rayon_robot > self.arene_longueur :
+        if self.robot.x - self.robot.rayon_robot < 0 or self.robot.x + self.robot.rayon_robot > self.arene_longueur :
             return True
-        if self.robot.y - self.robot.rayon_robot < 0 or self.y + self.robot.rayon_robot > self.arene_largeur :
+        if self.robot.y - self.robot.rayon_robot < 0 or self.robot.y + self.robot.rayon_robot > self.arene_largeur :
             return True
         
         # Vérifie une collision avec les obstacles
         for obj in self.obstacles:
-            if not(math.sqrt(((obj.x-robot.x)**2)+((obj.y-robot.y)**2)) > obj.rayon+robot.rayon_robot ): # si y'a colision il renvoie vrai
+            if not(math.sqrt(((obj.x-self.robot.x)**2)+((obj.y-self.robot.y)**2)) > obj.rayon+self.robot.rayon_robot ): # si y'a colision il renvoie vrai
                 return True
         
         return False
@@ -69,7 +69,7 @@ class Simulation :
         return -1 '''
     
     
-    def trace_cote_carre(self,robot, vitesse, dt, taille_carre):
+    def trace_cote_carre(self, vitesse, dt, taille_carre):
         """
         Trace un coté du carré avec le robot et faire une rotation pour se préparer à tourner.
     
@@ -82,27 +82,27 @@ class Simulation :
         Retourne:
             list: une liste de tuples représentant la position x, y du robot à chaque instant.
         """
-        robot.set_vitesse(vitesse, vitesse)
+        self.robot.set_vitesse(vitesse, vitesse)
         duree = taille_carre / ((robot.vitesse_roue_gauche + robot.vitesse_roue_droite) / 2) * robot.rayon_roue * 2 * math.pi
         nombre_pas = int(duree / dt)
         #le robot trace un coté du carré
         for j in range(nombre_pas):
             self.deplacement(dt)
-            if self.check_collision(self.robot):
-                robot.set_vitesse(0,0)
-                return robot.historique
+            if self.check_collision():
+                self.robot.set_vitesse(0,0)
+                return self.robot.historique
 
         
-        robot.set_vitesse(vitesse, -vitesse) #pour pouvoir tourner
+        self.robot.set_vitesse(vitesse, -vitesse) #pour pouvoir tourner
         angle = math.pi / 2
-        vitesse_angulaire = (robot.vitesse_roue_droite - robot.vitesse_roue_gauche) / (2 * math.pi * robot.distance_roues)
+        vitesse_angulaire = (self.robot.vitesse_roue_droite - self.robot.vitesse_roue_gauche) / (2 * math.pi * self.robot.distance_roues)
         temps_tourner = abs(angle / vitesse_angulaire) #temps nécessaire pour finir le tour
         nombre_pas = int(temps_tourner / dt)
         #rotation du robot autour de son axe central
         for j in range(nombre_pas):
             self.deplacement(dt)
 
-        return robot.historique
+        return self.robot.historique
     
     def simulation_carre(self,distance,time_delta):#prend une distance à parcourir 
         """ Fait faire un carré au robot dont les cotés sont égaux à la distance"""
@@ -110,7 +110,7 @@ class Simulation :
         get_dist=0.0
         state=0
         b=False # Variable qui permet de savoir si on a tourné dans la boucle 
-        while not(self.check_collision(self.robot) or state>4):
+        while not(self.check_collision() or state>4):
             if(get_dist>=distance):
                 b=True
                 self.robot.set_vitesse(-self.robot.vitesse_roue_gauche,self.robot.vitesse_roue_droite)
@@ -128,36 +128,24 @@ class Simulation :
                 self.robot.set_vitesse(-self.robot.vitesse_roue_gauche,self.robot.vitesse_roue_droite)
             self.deplacement(time_delta)
             get_dist+=math.sqrt((self.robot.x-self.robot.historique[len-2][0])**2+(self.robot.y-self.robot.historique[len-2][1])**2) # ajout de la distance entre l'ancienne position et la nouvelle position 
-
+        return self.robot.historique 
     def affichage(self):
         fig, ax = plt.subplots()
-        l_obstacles=[plt.Circle((obs.x,obs.y),obs.rayon)for obs in self.obstacles]
+        l_obstacles=[plt.Circle((obs.x,obs.y),obs.rayon)for obs in obstacles]
         for o in l_obstacles:
             ax.add_patch(o)
-        l_robot=[plt.Circle((x, y), 0.3, color='r') for (x,y) in self.robot.historique]
+        l_robot=[plt.Circle((x, y), 0.9, color='r') for (x,y) in simulation_historique]
         for r in l_robot:
             ax.add_patch(r)
-        ax.set_xlim(0, self.arene_longueur)
-        ax.set_ylim(0, self.arene_largeur)
+        ax.set_xlim(0, 50.0)
+        ax.set_ylim(0, 50.0)
         plt.show()
 
-robot = Robot(x=44.4, y=44.4, orientation=0, rayon_robot=22.3, rayon_roue=2.3, distance_roues=13.2) 
-obstacles= [Obstacle(random.uniform(0,100),random.uniform(0,100),random.uniform(3.78,6.65))for i in range (5) ]
-simulation=Simulation(obstacles)
+robot = Robot(x=50, y=50, orientation=0, rayon_robot=0.3, rayon_roue=0.05, distance_roues=0.2) 
+obstacles= [Obstacle(random.uniform(0,100),random.uniform(0,100),random.uniform(0.3,1))for i in range (50) ]
+simulation=Simulation(robot,obstacles)
 
-simulation_historique = simulation.trace_cote_carre(robot, 10, 0.0001, 10)
-simulation.affichage()
-"""simulation_historique = simulation.simulation_carre(10, 0.0001)
-simulation.affichage()"""
-
-            
-    
-
-
-
-           
-            
-
-
-
-
+while True:
+    simulation_historique = simulation.trace_cote_carre( 10, 0.0001, 10)
+    print(simulation_historique)
+    simulation.affichage() 
