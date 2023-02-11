23ec2c6497e517812572f8c4bdfb0c6a6c4e62ce
adelbnsl <96301345+adelbnsl@users.noreply.github.com>
2023-02-11 15:27:51 +0100

    Ajout du test unitaire de la methode check_collision

--- test_robot.py
+++ test_robot.py
@@ -1,10 +1,14 @@
 import unittest 
-from robot import Robot 
+from robot import Robot
+frome obstacle import Obstacle 
 class TestRobot(unittest.TestCase):
     def setUp(self):
         self.r=Robot(2,3,4.2,1,6,8)
     def test_set_vitesse(self):
         self.r.set_vitesse(6,5)
+    def test_check_collision(self):
+        self.r.check_collision(50,50,[Obstacle(4,4,5),Obstacle(8,6,2)])
+    
     
 
         
