a3a1166e25d52d8ca99f3ff5f9c4ab54b881918d
adelbnsl <96301345+adelbnsl@users.noreply.github.com>
2023-02-11 15:23:15 +0100

    Ajout du test unitaire de  la méthode set_vitesse

--- test_robot.py
+++ test_robot.py
@@ -3,6 +3,8 @@ from robot import Robot
 class TestRobot(unittest.TestCase):
     def setUp(self):
         self.r=Robot(2,3,4.2,1,6,8)
+    def test_set_vitesse(self):
+        self.r.set_vitesse(6,5)
     
 
         
