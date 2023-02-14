5b3b2117c7cc1345cb9b7a8baced3fd98dcbb950
URGaruda <daba.indigo@gmail.com>
2023-02-14 23:22:57 +0100

    Ajout d'un affichage graphique

--- affichage.py
+++ affichage.py
@@ -1,9 +1,22 @@
 from arene import Arene 
+import math
+import matplotlib.pyplot as plt
+from robot import Robot 
+import random
 class Affichage() : 
 
     def __init__(self,arene):
         self.arene=arene
 
-    def updateAffichage(self):
-
-        return  
+    def updateAffichage(self,robot,obstacles):
+        fig, ax = plt.subplots()
+        l_obstacles=[plt.Circle((obs.x,obs.y),obs.rayon)for obs in obstacles]
+        for o in l_obstacles:
+            ax.add_patch(o)
+        l_robot=[plt.Circle((robot.x,robot.y),robot.rayon_robot, color='r') ]
+        for r in l_robot:
+            ax.add_patch(r)
+        ax.set_xlim(0, 100)
+        ax.set_ylim(0, 100)
+        plt.show()
+      
--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import random
 from arene import Arene,Obstacle
 from robot import Robot 
 from affichage import Affichage 
+
 l_obstacle=[Obstacle(random.uniform(0,100),random.uniform(0,100),random.uniform(0.3,1))for i in range (30) ]
 dexter=Robot(55.56,26.78,0,0.3,0.05,0.2)
 terrain=Arene(dexter,l_obstacle,0.0001)
 ruby=ia.IA(dexter,10.5,terrain)
+aff=Affichage(terrain)
 ruby.start(5.4,9.7)
 while not ruby.stop():
     terrain.arene_update()
     ruby.step()
+    aff.updateAffichage(dexter,l_obstacle)
+    
 
