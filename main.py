2e76e7b85867ffd64c8a4e27edaeb188e29f39d4
URGaruda <daba.indigo@gmail.com>
2023-02-14 17:51:56 +0100

    petit changement sur main

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import random
 from arene import Arene,Obstacle
 from robot import Robot 
 from affichage import Affichage 
-l_obstacle=[Obstacle(random.uniform(0,100),random.uniform(0,100),random.uniform(0.3,1))for i in range (50) ]
-dexter=Robot(44.56,26.78,3,0.3,0.05,0.2)
+l_obstacle=[Obstacle(random.uniform(0,100),random.uniform(0,100),random.uniform(0.3,1))for i in range (30) ]
+dexter=Robot(55.56,26.78,0,0.3,0.05,0.2)
 terrain=Arene(dexter,l_obstacle,0.0001)
 ruby=ia.IA(dexter,10.5,terrain)
-ruby.start(15.4,16.7)
+ruby.start(5.4,9.7)
 while not ruby.stop():
     terrain.arene_update()
     ruby.step()
-    
+
