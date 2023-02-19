6018dc70bbaa5f7eb62fc859af90ea15e5aa5450
djida-mak <djidamakhlouf@outlook.fr>
2023-02-19 21:11:06 +0100

    classe robomockup

---
+++ robotmockup.py
@@ -0,0 +1,8 @@
+import time
+import math
+ 
+class robotmockup:
+    WHEEL_BASE_WIDTH         = 117  
+    WHEEL_DIAMETER           = 66.5 
+    WHEEL_BASE_CIRCUMFERENCE = WHEEL_BASE_WIDTH * math.pi 
+    WHEEL_CIRCUMFERENCE      = WHEEL_DIAMETER   * math.pi
