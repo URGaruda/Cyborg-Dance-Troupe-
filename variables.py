6e78828cce8e1286889a03f68392af089a165077
URGaruda <101092183+URGaruda@users.noreply.github.com>
2023-02-22 10:46:59 +0100

    Delete variables.py

--- variables.py
+++
@@ -1,6 +0,0 @@
-import constantes 
-from robot import Robot
-from intermediaire import Intermediaire
-"""Possede les objets des classes qu'on veut pouvoir manipuler sur plusieurs fichiers """
-dexter=Robot()
-inter=Intermediaire(dexter)
