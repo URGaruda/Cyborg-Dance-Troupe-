cb2cef146831e5b194a0b72ed02ce23878a2a669
DABA <21110908@ppti-14-406-15.ufr-info-p6.jussieu.fr>
2023-02-22 10:21:36 +0100

    changement sur ia_seq

--- ia_seq.py
+++ ia_seq.py
@@ -1,22 +1,21 @@
 import ia
 import ia_tourner 
-
+from intermediaire import Intermediaire 
 class IA_Seq:
     def __init__(self,liste_ia):
         self.liste=liste_ia
-        
     def start(self):
         self.cur=0
 
     def step(self):
+        if self.liste[self.cur].stop() :
+            self.cur+=1
+            self.liste[self.cur].start()
+        
         self.liste[self.cur].step()
 
     def stop(self):
         return self.cur>=len(self.liste)
     
-    def update(self):
-        """ fait la maj de l'ia actuelle de la liste  """
-        if self.liste[self.cur].stop() :
-            self.cur+=1
-            self.liste[self.cur].start()
+    
         
