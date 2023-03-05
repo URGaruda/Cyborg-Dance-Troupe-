2b859cfaf40bd4c78b71ca39815241bd713e4969
URGaruda <daba.indigo@gmail.com>
2023-03-05 15:00:29 +0100

    diminution du nbr d'obstacle du main

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import time
 from Robot_Arene_Obstacle.robot import Robot
 from Robot_Arene_Obstacle.intermediaire import Intermediaire  
 
-l_obstacle=[Obstacle(random.uniform(0,Arene.arene_longueur),random.uniform(0,Arene.arene_largeur),random.uniform(2.9,10))for i in range (30) ]
+l_obstacle=[Obstacle(random.uniform(0,Arene.arene_longueur),random.uniform(0,Arene.arene_largeur),random.uniform(2.9,10))for i in range (10) ]
 dexter=Robot()
 inter=Intermediaire(dexter)
 
