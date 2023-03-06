05c061478e8ca8a14a5ffa28e78667a2327cbe83
URGaruda <daba.indigo@gmail.com>
2023-03-06 11:51:00 +0100

    chagement de l'affichage

--- Interfaces/affichage.py
+++ Interfaces/affichage.py
@@ -13,6 +13,7 @@ class Affichage() :
         self.acanvas= Canvas(self.fenetre,width = Arene.arene_longueur*3, height = Arene.arene_largeur*3 , bd=0, bg="white")
         self.acanvas.pack(padx=10,pady=10)
         self.liste_objet=[]
+        self.liste_fleche=[]
     def clear(self):
         self.acanvas.delete(ALL)
         self.liste_objet=[]
@@ -22,12 +23,15 @@ class Affichage() :
         if len(self.liste_objet)>0:
             self.acanvas.delete(self.liste_objet[-1])
             self.liste_objet.pop()
+        if len(self.liste_fleche)>0:
+            self.acanvas.delete(self.liste_fleche[-1])
+            self.liste_fleche.pop()
         self.liste_objet.append(self.acanvas.create_oval((robot.x + ray_r)*2.5, (robot.y + ray_r)*2.5,( robot.x - ray_r)*2.5, (robot.y - ray_r)*2.5, fill='red'))
         x1= robot.x *2.5
         y1 = robot.y *2.5
         x2 = x1 + + ray_r *2.5 * math.cos(robot.orientation)
         y2 = y1 + + ray_r *2.5 * math.sin(robot.orientation)
-        self.acanvas.create_line(x1,y1,x2,y2,arrow=LAST,fill='blue')
+        self.liste_fleche.append(self.acanvas.create_line(x1,y1,x2,y2,arrow=LAST,fill='blue'))
         for o in obstacles:
             self.acanvas.create_oval((o.x + o.rayon)*2.5,(o.y + o.rayon)*2.5,(o.x - o.rayon)*2.5,( o.y - o.rayon)*2.5, fill='green')
         self.fenetre.update()
