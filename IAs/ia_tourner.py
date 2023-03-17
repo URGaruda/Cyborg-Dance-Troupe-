34089db709817cc1499d47769943ae0052f313c3
URGaruda <daba.indigo@gmail.com>
2023-03-17 18:18:05 +0100

    changement de vitesse d'ia tourner

--- IAs/ia_tourner.py
+++ IAs/ia_tourner.py
@@ -11,12 +11,13 @@ class Ia_Tourner:
         self.limite=False
     def start(self):
         self.a_tourner=0.0
-        self.inter.tourner_gauche(self.vitesse)
+        self.inter.tourner_gauche(self.vitesse/7)
     def step(self):
         self.a_tourner+=self.inter.get_angle()
+        """
         if((self.a_tourner/self.angle)*100>=75.0) and self.limite==False :
             self.inter.tourner_gauche(self.vitesse/2)
             self.limite=True
-        
+        """
     def stop(self):
         return self.a_tourner>=self.angle
