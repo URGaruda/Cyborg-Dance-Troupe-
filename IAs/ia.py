ac5e2f67fec1499a41fd2b34bc9892f230e82f74
URGaruda <101092183+URGaruda@users.noreply.github.com>
2023-03-17 17:05:50 +0100

    Update ia.py

--- IAs/ia.py
+++ IAs/ia.py
@@ -1,4 +1,4 @@
-from Robot_Arene_Obstacle.intermediaire import Intermediaire
+from .intermediaire import Intermediaire
 import math
 
 class IA:
@@ -7,10 +7,14 @@ class IA:
         self.parcouru=0.0
         self.inter=dexter
         self.vitesse=vitesse
+        self.limite=False
     def start(self):
         self.parcouru=0.0
-        self.inter.avancer(self.vitesse)
+        self.inter.avancer(self.vitesse,self.vitesse)
     def step(self):
         self.parcouru+=self.inter.get_distance()
+        if((self.parcouru/self.distance)*100>=85.0) and self.limite==False :
+            self.inter.avancer(self.vitesse/1.3,self.vitesse/1.3)
+            self.limite=True
     def stop(self):
         return self.parcouru>=self.distance 
