226b2042e01b13bd64092c2daad0f874c10acb21
adelbnsl <adelbnsl@gmail.com>
2023-03-17 18:08:44 +0100

    Ajout test unitaire class arene

---
+++ Tests_Unitaire/test_arene.py
@@ -0,0 +1,10 @@
+import unittest 
+from Robot_Arene_Obstacle.arene import Arene
+
+class TestArene(unittest.TestCase):
+    def test_check_collision(self):
+        self.a.check_collision();
+    def test_ajout_obstacle(self):
+        self.a.ajout_obstacles(7)
+
+    
