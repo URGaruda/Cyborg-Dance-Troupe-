40f17a42b5851767be98b23d9975ab85ec92d74a
adelbnsl <adelbnsl@gmail.com>
2023-03-17 18:18:01 +0100

    Ajout test unitaire de la classe Affichage

---
+++ Tests_Unitaire/test_affichage.py
@@ -0,0 +1,5 @@
+import unittest
+from Interfaces.affichage import Affichage
+from Robot_Arene_Obstacle.obstacle import Obstacle 
+class TestAffichage(unittest.TestCase):
+    def test_updateaffichage(Robot(),Obstacle(7,3,42))
