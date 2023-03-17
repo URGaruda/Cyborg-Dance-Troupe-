ee82a9f787192d092129009dc72e34cd82b038ea
adelbnsl <adelbnsl@gmail.com>
2023-03-17 18:27:30 +0100

    Correction des erreurs sur les test unitaires

--- Tests_Unitaire/test_affichage.py
+++ Tests_Unitaire/test_affichage.py
@@ -1,5 +1,8 @@
 import unittest
 from Interfaces.affichage import Affichage
 from Robot_Arene_Obstacle.obstacle import Obstacle 
+from Robot_Arene_Obstacle.robot import Robot
 class TestAffichage(unittest.TestCase):
-    def test_updateaffichage(Robot(),Obstacle(7,3,42))
+    def test_updateaffichage(self):
+        self.af.updateaffichage(Robot(),Obstacle(7,3,42))
+        
--- Tests_Unitaire/test_arene.py
+++ Tests_Unitaire/test_arene.py
@@ -3,7 +3,7 @@ from Robot_Arene_Obstacle.arene import Arene
 
 class TestArene(unittest.TestCase):
     def test_check_collision(self):
-        self.a.check_collision();
+        self.a.check_collision()
     def test_ajout_obstacle(self):
         self.a.ajout_obstacles(7)
 
