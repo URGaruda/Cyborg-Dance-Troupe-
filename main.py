179d7dca110e963931f2dfba7444eba713b0cb7e
URGaruda <101092183+URGaruda@users.noreply.github.com>
2023-03-17 17:10:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,23 @@ from Robot_Arene_Obstacle.obstacle import Obstacle
 from Interfaces.affichage import Affichage
 import Autres.constantes as constantes 
 import time
-from Robot_Arene_Obstacle.robot import Robot 
-from Robot_Arene_Obstacle.intermediaire import Intermediaire
+from Robot_Arene_Obstacle.robot import Robot
+from IAs.intermediaire import Intermediaire  
 
 l_obstacle=[Obstacle(random.uniform(0,Arene.arene_longueur),random.uniform(0,Arene.arene_largeur),random.uniform(2.9,10))for i in range (10) ]
-dexter=Intermediaire(Robot())
+dexter=Robot()
+inter=Intermediaire(dexter)
 
-terrain=Arene(dexter,l_obstacle)
-ruby=ia.IA(dexter,400.5,constantes.Vitesse)
+terrain=Arene(inter,l_obstacle)
+ruby=ia.IA(inter,40.5,constantes.Vitesse)
 aff=Affichage(terrain)
 
 l_ia=[]
-for i in range(7):
+for i in range(9):
     if(i%2==0):
-        l_ia.append(ia.IA(dexter,55.5,constantes.Vitesse) )
+        l_ia.append(ia.IA(inter,25.5,constantes.Vitesse) )
     else:
-       l_ia.append( ia_tourner.Ia_Tourner(dexter,90,constantes.Vitesse*1.90) )
+       l_ia.append( ia_tourner.Ia_Tourner(inter,90,constantes.Vitesse ))
 ia_carre=ia_seq.IA_Seq(l_ia)
 
 
@@ -35,8 +36,9 @@ while not ruby.stop() and not terrain.check_collision():
     #time.sleep(0.2)
 """
 ia_carre.start()
+inter.start_time()
 while not ia_carre.stop() and not terrain.check_collision():
     terrain.arene_update()
     ia_carre.step()
-    aff.updateAffichage(dexter,l_obstacle)
+    aff.updateAffichage(inter,l_obstacle)
     aff.fenetre.update()
