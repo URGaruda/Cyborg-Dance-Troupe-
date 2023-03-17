ed75a55bbde29f82bed9116987771f296a99180b
adelbnsl <adelbnsl@gmail.com>
2023-03-17 18:33:14 +0100

    Ajout du test de get angle

--- Tests_Unitaire/test_intermediaire.py
+++ Tests_Unitaire/test_intermediaire.py
@@ -3,3 +3,5 @@ from IAs.intermediaire import Intermediaire
 class TestIntermediare(unittest.TestCase):
     def test_get_distance(self):
         self.i.get_distance()
+    def test_get_angle(self):
+        self.i.get_angle()
