566e267fcb348ecd27f955260351595ee950948e
adelbnsl <adelbnsl@gmail.com>
2023-03-17 17:59:19 +0100

    Ajout du test unitaire de  senseur_distance

--- Tests_Unitaire/test_robot.py
+++ Tests_Unitaire/test_robot.py
@@ -1,15 +1,15 @@
 import unittest 
-from robot import Robot
-from obstacle import Obstacle 
+from Robot_Arene_Obstacle.robot import Robot
+from Robot_Arene_Obstacle.obstacle import Obstacle 
 class TestRobot(unittest.TestCase):
     def setUp(self):
         self.r=Robot(2,3,4.2,1,6,8)
     def test_set_vitesse(self):
         self.r.set_vitesse(6.4,5.6)
-    def test_check_collision(self):
-        self.r.check_collision(50.0,50.0,[Obstacle(4.0,4.0,5.0),Obstacle(8.0,6.0,2.0)])
     def test_deplacement(self):
         self.r.deplacement(0.3)
+    def test_senseur_distance(self):
+        self.r.senseur_distance([Obstacle(4.0,4.0,5.0),Obstacle(8.0,6.0,2.0)])
 
 
 
