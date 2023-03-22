725645dd1adbc04a886449316f30ffb077fb9680
URGaruda <101092183+URGaruda@users.noreply.github.com>
2023-03-22 09:24:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,35 +10,43 @@ import time
 from CDT.Robot_Arene_Obstacle.robot import Robot
 from CDT.IAs.intermediaire import Intermediaire  
 
-l_obstacle=[Obstacle(random.uniform(0,Arene.arene_longueur),random.uniform(0,Arene.arene_largeur),random.uniform(2.9,10))for i in range (10) ]
-dexter=Robot()
-inter=Intermediaire(dexter)
+def make_line():
+    l_obstacle=[Obstacle(random.uniform(0,Arene.arene_longueur),random.uniform(0,Arene.arene_largeur),random.uniform(2.9,10))for i in range (10) ]
+    dexter=Robot()
+    inter=Intermediaire(dexter)
+    terrain=Arene(inter,l_obstacle)
+    ruby=ia.IA(inter,40.5,constantes.Vitesse)
+    aff=Affichage(terrain)
+    #simulation 
+    ruby.start()
+    inter.start_time()
+    while not ruby.stop() and not terrain.check_collision():
+        terrain.arene_update()
+        ruby.step()
+        aff.updateAffichage(inter,l_obstacle)
 
-terrain=Arene(inter,l_obstacle)
-ruby=ia.IA(inter,40.5,constantes.Vitesse)
-aff=Affichage(terrain)
+def make_carre():
+    l_obstacle=[Obstacle(random.uniform(0,Arene.arene_longueur),random.uniform(0,Arene.arene_largeur),random.uniform(2.9,10))for i in range (10) ]
+    dexter=Robot()
+    inter=Intermediaire(dexter)
 
-l_ia=[]
-for i in range(9):
-    if(i%2==0):
-        l_ia.append(ia.IA(inter,25.5,constantes.Vitesse) )
-    else:
-       l_ia.append( ia_tourner.Ia_Tourner(inter,90,constantes.Vitesse ))
-ia_carre=ia_seq.IA_Seq(l_ia)
+    terrain=Arene(inter,l_obstacle)
+    ruby=ia.IA(inter,40.5,constantes.Vitesse)
+    aff=Affichage(terrain)
 
-
-"""
-ruby.start()
-while not ruby.stop() and not terrain.check_collision():
-    terrain.arene_update()
-    ruby.step()
-    aff.updateAffichage(dexter,l_obstacle)
-    #time.sleep(0.2)
-"""
-ia_carre.start()
-inter.start_time()
-while not ia_carre.stop() and not terrain.check_collision():
-    terrain.arene_update()
-    ia_carre.step()
-    aff.updateAffichage(inter,l_obstacle)
-    aff.fenetre.update()
+    l_ia=[]
+    for i in range(9):
+        if(i%2==0):
+            l_ia.append(ia.IA(inter,25.5,constantes.Vitesse) )
+        else:
+            l_ia.append( ia_tourner.Ia_Tourner(inter,90,constantes.Vitesse ))
+    ia_carre=ia_seq.IA_Seq(l_ia)
+    ia_carre.start()
+    inter.start_time()
+    while not ia_carre.stop() and not terrain.check_collision():
+        terrain.arene_update()
+        ia_carre.step()
+        aff.updateAffichage(inter,l_obstacle)
+        aff.fenetre.update()
+        
+make_carre() 
