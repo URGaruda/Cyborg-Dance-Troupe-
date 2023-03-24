fb4449a7958abe73765a45523f0b4e062c23255f
URGaruda <daba.indigo@gmail.com>
2023-03-24 10:59:09 +0100

    modification du mockup

--- CDT/Autres/robotmockup.py
+++ CDT/Autres/robotmockup.py
@@ -6,37 +6,17 @@ class robotmockup:
     WHEEL_BASE_CIRCUMFERENCE = WHEEL_BASE_WIDTH * math.pi 
     WHEEL_CIRCUMFERENCE      = WHEEL_DIAMETER   * math.pi
 
-    def __init__(self,controleur,fps=25,resolution=None,servoPort ="SERVO1",motionPort="AD1"):
-        self._gpg= EasyGoPiGo3()
+    def __init__(self,controleur,fps=25):
         self.controleur=controleur
         self.fps=fps
-        self.LED_LEFT_EYE = self._gpg.LED_LEFT_EYE
-        self.LED_RIGHT_EYE = self._gpg.LED_RIGHT_EYE
-        self.LED_LEFT_BLINKER = self._gpg.LED_LEFT_BLINKER
-        self.LED_RIGHT_BLINKER = self._gpg.LED_RIGHT_BLINKER
-        self.LED_WIFI = self._gpg.LED_WIFI
-        self.MOTOR_LEFT= self._gpg.MOTOR_LEFT
-        self.MOTOR_RIGHT = self._gpg.MOTOR_RIGHT
+        self.LED_LEFT_EYE = 15
+        self.LED_RIGHT_EYE = 22
+        self.LED_LEFT_BLINKER = 10
+        self.LED_RIGHT_BLINKER = 10
+        self.LED_WIFI = 0
+        self.MOTOR_LEFT= 0
+        self.MOTOR_RIGHT = 0
 
-        try:
-            self.camera=picamera.PICamera()
-            if resolution:
-                self.camera.resolution =resolution
-        except Exception as e:
-            print("Camera not found", e)
-        try:
-            self.servo =Servo(servoPort,self._gpg)
-        except Exception as e:
-            print("servo not found",e)
-        try:
-            self.distanceSensor = ds_sensor.DistanceSensor()
-        except Exception as e:
-            print("Distance Sensor not found",e)
-        try:
-            self.imu = imu.inertial_measurement_unit()
-        except Exception as e:
-            print("IMU sensor not found",e)
-        self._gpg.set_motor_limits(self._gpg.MOTOR_LEFT+self._gpg.MOTOR_RIGHT,0)
     
     def set_led(self, led, red = 0, green = 0, blue = 0):
         pass
@@ -48,14 +28,15 @@ class robotmockup:
         pass
 
 
+
     def get_motor_position(self):
-        pass
+        return(73.4,46.8)
    
     def offset_motor_encoder(self, port, offset):
         pass
 
     def get_distance(self):
-        pass
+        return 30*4
 
     def servo_rotate(self,position):
         pass
@@ -64,5 +45,5 @@ class robotmockup:
         pass
 
     def get_image(self):
-        pass
+        return 500
     
