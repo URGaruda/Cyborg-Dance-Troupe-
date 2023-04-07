1d12de4858ff14d53085852f96c0ddd6c3942cba
URGaruda <daba.indigo@gmail.com>
2023-04-07 23:30:52 +0200

    modifiaction sur ia tourner

--- CDT/IAs/ia_tourner.py
+++ CDT/IAs/ia_tourner.py
@@ -11,15 +11,15 @@ class Ia_Tourner:
         self.inter.angleP=0.0
         self.a_tourner=0.0
         self.inter.start_time_angle()
-        self.inter.tourner_gauche(self.vitesse/13)
+        self.inter.tourner_gauche(self.vitesse)
     def step(self):
         #print("angle :",self.a_tourner)
         self.a_tourner=self.inter.get_angle()
-        """
-        if((self.a_tourner/self.angle)*100>=92.0) and self.limite==False : # à utiliser que si l'affichage est utilisé dans le main 
-            self.inter.tourner_gauche(self.vitesse/75)
+        
+        if((self.a_tourner/self.angle)*100>=82.0) and self.limite==False : # à utiliser que si l'affichage est utilisé dans le main 
+            self.inter.tourner_gauche(self.vitesse/33)
             self.limite=True
-        """
+        
         
     def stop(self):
         return self.a_tourner>self.angle
