aa0baa9335890572731d92e0352eafc122058c00
adelbnsl <adelbnsl@gmail.com>
2023-04-11 20:35:13 +0200

    Test unitaire intermediare

--- CDT/Tests_Unitaire/test_intermediaire.py
+++ CDT/Tests_Unitaire/test_intermediaire.py
@@ -1,6 +1,10 @@
 import unittest
 from IAs.intermediaire import Intermediaire
 class TestIntermediare(unittest.TestCase):
+    def test_start_time_dist(self):
+        self.i.start_time_dist()
+    def test_start_time_angle(self):
+        self.i.start_time_angle()
     def test_get_distance(self):
         self.i.get_distance()
     def test_get_angle(self):
