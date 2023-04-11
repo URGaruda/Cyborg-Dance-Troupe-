32e5ad621d63691adfe8dece53558425c7becf2c
adelbnsl <adelbnsl@gmail.com>
2023-04-11 20:46:52 +0200

    Test unitaire robot

--- CDT/Tests_Unitaire/test_robot.py
+++ CDT/Tests_Unitaire/test_robot.py
@@ -3,13 +3,18 @@ from CDT.Simulation.robot import Robot
 from CDT.Simulation.obstacle import Obstacle 
 class TestRobot(unittest.TestCase):
     def setUp(self):
-        self.r=Robot(2,3,4.2,1,6,8)
+        self.r=Robot()
+    def test_start_time(self):
+        self.r.start_time()
     def test_set_vitesse(self):
-        self.r.set_vitesse(6.4,5.6)
-    def test_check_collision(self):
-        self.r.check_collision(50.0,50.0,[Obstacle(4.0,4.0,5.0),Obstacle(8.0,6.0,2.0)])
+        self.r.set_vitesse(6.4,5)
     def test_deplacement(self):
         self.r.deplacement(0.3)
+    def test_senseur_distance(self):
+        self.r.senseur_distance([Obstacle(4.0,4.0,5.0),Obstacle(8.0,6.0,2.0)])
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 
