ffd509bfcfb32691e541c8c31ef0e1a2ce930664
adelbnsl <adelbnsl@gmail.com>
2023-04-12 09:44:40 +0200

    Ajout du main du vrai robot

--- essai_robot.py
+++ essai_robot.py
@@ -1,29 +1,18 @@
 from CDT.IAs.inter_robot import Inter_Robot
-from CDT.Weiter.robotmockup import Robotmockup 
 import CDT.IAs.ia as ia 
 import CDT.IAs.ia_tourner as ia_tourner
 import CDT.IAs.ia_seq as ia_seq 
-import random
-from CDT.Simulation.arene import Arene
-from CDT.Simulation.obstacle import Obstacle
-from CDT.Interfaces.affichage import Affichage
 import CDT.Weiter.constantes as constantes 
-import time
-from CDT.Simulation.robot import Robot
-from CDT.IAs.intermediaire import Intermediaire  
+import robot2IN013 as Robot
 
-
-l_obstacle=[]
 dexter=None
 inter=None
 ruby=None
 l_ia=[]
 main_ia=None
 def initiate(nb_obstacle):
-    global l_obstacle
-    l_obstacle=[Obstacle(random.uniform(0,Arene.arene_longueur),random.uniform(0,Arene.arene_largeur),random.uniform(2.9,10))for i in range (nb_obstacle) ]
     global dexter
-    dexter=Robotmockup()
+    dexter=Robot()
     global inter
     inter=Inter_Robot(dexter)
 
@@ -71,6 +60,19 @@ def create_triangle_equilateral(distance):
             l_ia.append(ia.IA(inter,distance,constantes.Vitesse) )
         else:
             l_ia.append( ia_tourner.Ia_Tourner(inter,120,constantes.Vitesse ))
+    ia_equi=ia_seq.IA_Seq(l_ia)
+    global main_ia
+    main_ia=ia_equi
+def create_hexagone(distance):
+    global l_ia
+    l_ia.append(ia_tourner.Ia_Tourner(inter,60,constantes.Vitesse ))
+    for i in range(11):
+        if(i%2==0):
+            l_ia.append(ia.IA(inter,distance,constantes.Vitesse) )
+            
+        else:
+            l_ia.append( ia_tourner.Ia_Tourner(inter,60,constantes.Vitesse ))
+            
     ia_equi=ia_seq.IA_Seq(l_ia)
     global main_ia
     main_ia=ia_equi
